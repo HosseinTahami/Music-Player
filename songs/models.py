4d16ea92b1967381b5914297e1019c2e45294027
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 11:08:58 +0330

    fix: '128' --> 128

--- songs/models.py
+++ songs/models.py
@@ -12,11 +12,11 @@ class Genre(models.Model):
 
 
 class Song(models.Model):
-    name = models.CharField(max_length="128")
+    name = models.CharField(max_length=128)
     cover_img = models.ImageField("song_cover_images/")
     description = models.TextField(default=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    audio_file = models.FieldFile(upload_to="song_files/")
+    audio_file = models.FileField(upload_to="songs/song_files/")
     genres = models.ManyToManyField(Genre)
 
     def __str__(self) -> str:
