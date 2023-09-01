735d3990960cc3353cb564bf2f43bc66d00f0ca4
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 10:09:29 +0330

    Feat: interactions app created

--- songs/models.py
+++ songs/models.py
@@ -14,25 +14,25 @@ class Genre(models.Model):
         return f"Name: {self.name}"
 
 
-class Song(models.Model):
-    name = models.CharField(max_length="128")
-    cover_img = models.ImageField("song_cover_images/")
-    description = models.TextField(default=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    audio_file = models.FieldFile(upload_to="song_files/")
-    genres = models.ManyToManyField(Genre)
+# class Song(models.Model):
+#     name = models.CharField(max_length="128")
+#     cover_img = models.ImageField("song_cover_images/")
+#     description = models.TextField(default=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     audio_file = models.FieldFile(upload_to="song_files/")
+#     genres = models.ManyToManyField(Genre)
 
-    def __str__(self) -> str:
-        return f"id: {self.id} || name: {self.name}"
+#     def __str__(self) -> str:
+#         return f"id: {self.id} || name: {self.name}"
 
 
 class Playlist(models.Model):
     name = models.CharField(max_length=128)
     cover_img = models.ImageField(upload_to="playlist_cover_images/")
     description = models.TextField(default=True, null=True)
-    songs = models.ManyToManyField(Song)
+    # songs = models.ManyToManyField(Song)
     created_at = models.DateField(auto_now_add=True)
-    owner = models.ForeignKey(Listener)
+    # owner = models.ForeignKey(Listener)
 
     def __str__(self) -> str:
         return f"Name: {self.name}"
