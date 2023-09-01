f2ecb1aefbabbea23cfd62f4159f44ec7a139266
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 18:02:18 +0330

    Refactor: UserModels Edited

--- accounts/models.py
+++ accounts/models.py
@@ -6,25 +6,30 @@ then Artist & Listener will inheritance it !
 # Django Imports
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.mixins import PermissionMixin
+
 
 # Inside Project Imports
 from songs.models import Song
+from managers import CustomUserManager
 
 
-class CustomAbstractBaseUser(AbstractBaseUser):
+class CustomAbstractBaseUser(AbstractBaseUser, PermissionMixin):
     username = models.CharField(max_length=28, unique=True)
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.EmailField(unique=True)
     gender = models.BooleanField(default=True)
     bio = models.TextField(blank=True, null=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True, auto_now_add=True, editable=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now=True, editable=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
 
     def __str__(self) -> str:
         return f"Name: {self.first_name} {self.last_name} || Email:{self.email}"
@@ -47,5 +52,5 @@ class Artist(CustomAbstractBaseUser):
 
 class Band(models.Model):
     name = models.CharField(max_length=128)
-    started_at = models.DateTimeField()
-    end_at = models.DateTimeField()
+    started_at = models.DateTimeField(auto_now_add=True, editable=False)
+    end_at = models.DateTimeField(null=True, blank=True, editable=True)
