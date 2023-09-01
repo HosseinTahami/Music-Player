80cc7ffdfe056de86dd71fbcafe264a8ce77225d
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 06:03:34 +0330

    Feat: Song Model Created

--- Home/models.py
+++ Home/models.py
@@ -8,3 +8,16 @@ class Genre(models.Model):
 
     def __str__(self) -> str:
         return f"Name: {self.name}"
+
+
+class Song(models.Model):
+    name = models.CharField(max_length="128")
+    cover_img = models.ImageField("song_cover_images/")
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    audio_file = models.FieldFile(upload_to="song_files/")
+    genres = models.ManyToManyField(Genre)
+    # artist =
+
+    def __str__(self) -> str:
+        return f"id: {self.id} || name: {self.name}"
