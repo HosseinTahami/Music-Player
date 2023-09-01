cfe6142f1815a3a8014c1ecd34d760c5ad5b2df8
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 10:20:38 +0330

    Feat: Playlist model created

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import Listener
+from songs.models import Song
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=128)
+    cover_img = models.ImageField(upload_to="playlist_cover_images/")
+    description = models.TextField(default=True, null=True)
+    songs = models.ManyToManyField(Song)
+    created_at = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(Listener)
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}"
+
+
+class Like(models.Model):
+    song = models.ForeignKey(Song)
+    liker = models.ForeignKey(Listener)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class comment(models.Model):
+    song = models.ForeignKey(Song)
+    created_at = models.DateTimeField(auto_now_add=True)
