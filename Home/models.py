509f10a1abe85981d20eefea1c98ea742df702f4
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 05:57:36 +0330

    Feat: Genre model created

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=128)
+    info = models.TextField()
+    genre_img = models.ImageField()
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}"
