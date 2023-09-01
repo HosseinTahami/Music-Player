6f34a9ab3d191ab176662e30d877e8c801a576a7
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 07:15:47 +0330

    Feat: Artist User Created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class Artist(AbstractBaseUser):
+    username = models.CharField(max_length=28, unique=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField()
+    gender = models.BooleanField()
+    bio = models.CharField(max_length=128)
+    profile_img = models.ImageField(upload_to="artists_images/")
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_alive = models.BooleanField(default=True)
+
+    USERNAME_FIELD = [
+        "username",
+        "email",
+    ]
+
+    def __str__(self) -> str:
+        return f"Name: {self.first_name} {self.last_name} || Email:{self.email}"
