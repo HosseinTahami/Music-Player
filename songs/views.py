6988f15d00780e085b80d5396765d5a09b74c2d1
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-01 09:52:26 +0330

    refactor: Home --> songs

--- songs/views.py
+++ songs/views.py
@@ -4,4 +4,4 @@ from django.views import View
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "Home/home.html")
+        return render(request, "songs/home.html")
