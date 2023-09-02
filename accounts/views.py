4b28e192486282dfe2977def4cb4657ab18f8fdb
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-02 12:00:53 +0330

    Feat: RegisterView Created

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+# Django Imports
+from django.shortcuts import render, redirect
 from django.views import View
 
+# Inside Project Imports
+from .forms import RegisterForm
+from .models import Listener, Artist
 
-class SignUpView(View):
+
+class RegisterView(View):
+    form_class = RegisterForm
+    template_name = "accounts/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("songs:home")
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request, *args, **kwargs):
-        pass
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
 
     def post(self, request, *args, **kwargs):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid:
+            cd = form.cleaned_data
+        if cd["user_type"] == "L":
+            Listener.objects.create()
+        else:
+            pass
